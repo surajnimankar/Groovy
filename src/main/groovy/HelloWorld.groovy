5a680774b4741da1e065ec0d7fa7813dc94748e7
Suraj Nimankar <surajnimankar@gmail.com>
2017-11-24 12:37:40 +0530

    Few small assignments added

--- src/main/groovy/HelloWorld.groovy
+++ src/main/groovy/HelloWorld.groovy
@@ -1,2 +1,13 @@
+import org.joda.time.*
+import org.joda.time.format.DateTimeFormat
+
+
 println("Hello Groovy World...!")
-println "today's dae is: " + new Date()
+println "today's date is: " + new Date()
+
+
+def prinatabeDate = new DateTime(new Date())
+def format = DateTimeFormat.forPattern('MM/dd/yyyy - hh:mm aa')
+
+println prinatabeDate.toString(format)
+
